f0d626d4f255e33a3cce56bd65f6a2a274737300
김현화 <h3652k@gmail.com>
2020-05-18 11:40:55 +0900

    [test] test swagger

--- gaenolja/src/main/java/com/gaenolja/controller/HotelController.java
+++ gaenolja/src/main/java/com/gaenolja/controller/HotelController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,11 +23,11 @@ public class HotelController {
 		return handleFail(e.getMessage(), HttpStatus.OK);
 	}
 	
-//	@PostMapping("/api/finder")
-//	@ApiOperation("font finder result")
-//		public ResponseEntity<Map<String, Object>> fontfinder(@RequestBody byte[] files){
-//		return handleSuccess(fontService.fontfinder(files));
-//	}
+	@GetMapping("/api/test")
+	@ApiOperation("swagger test")
+		public ResponseEntity<Map<String, Object>> fontfinder(){
+		return handleSuccess("hi");
+	}
 	
 	public ResponseEntity<Map<String, Object>> handleSuccess(Object data){
 		Map<String, Object> resultMap = new HashMap<String, Object>();
