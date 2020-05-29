33fced1f7eabfd0563eaba3bb5a172849fd93aa4
jngmk <rlawjd7186@naver.com>
2020-05-30 02:10:03 +0900

    [feat] add map feature   - make a hotel list view pager   - put the marker on the map   - link marker with viwe pager

--- front/app/src/main/java/com/example/dogplay/Service.kt
+++ front/app/src/main/java/com/example/dogplay/Service.kt
@@ -1,6 +1,8 @@
 package com.example.dogplay
 
 import com.google.gson.annotations.SerializedName
+import com.google.gson.internal.LinkedTreeMap
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.http.*
 import retrofit2.http.Header
@@ -18,6 +20,26 @@ data class HotelDetailDTO(
     var data:HashMap<String,Any>
 )
 
+data class HotelNearByDTO(
+    @SerializedName("data")
+    val data: ArrayList<HotelInfo>,
+    @SerializedName("state")
+    val state: String
+)
+
+data class HotelInfo(
+    val hotelnumber: Int = 0,
+    val userid: String,
+    val hashid: String,
+    val hotelname: String,
+    val latitude: Double,
+    val longitude: Double,
+    val address: String,
+    val contact: String,
+    val info: String,
+    val detail: ArrayList<ArrayList<String>>
+)
+
 
 interface Service {
     @GET("/api/v1/hotelstar/searchall")
@@ -27,6 +49,13 @@ interface Service {
     fun searchHotelDetail(
         @Query("hotelnumber") hotelnumber: Number
     ):Call<HotelDetailDTO>
+
+    @GET("/api/v1/hotel/search/latitude/longitude/distance")
+    fun getHotelNearBy(
+        @Query("distance") distance: Int,
+        @Query("latitude") latitude: Double,
+        @Query("longitude") longitude: Double
+    ): Call<HotelNearByDTO>
 
     @FormUrlEncoded
     @POST("/api/v1/hotel/insert")
