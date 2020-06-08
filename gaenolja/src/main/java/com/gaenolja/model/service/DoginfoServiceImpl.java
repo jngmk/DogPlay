04d177a9878921ed51b667c3d073a65b83af2195
김현화 <h3652k@gmail.com>
2020-06-08 10:25:41 +0900

    [fix] doginfo

--- gaenolja/src/main/java/com/gaenolja/model/service/DoginfoServiceImpl.java
+++ gaenolja/src/main/java/com/gaenolja/model/service/DoginfoServiceImpl.java
@@ -1,6 +1,9 @@
 package com.gaenolja.model.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +22,21 @@ public class DoginfoServiceImpl implements DoginfoService {
 	public List<Doginfo> searchall(){
 		try {
 			List<Doginfo> doginfo = dao.searchall();
+			for (Doginfo dog:doginfo) {
+				if (!Objects.isNull(dog.getDetail())) {
+					Gson gson = new Gson();
+					HashMap<String, String> jsonObject = gson.fromJson(dog.getDetail().toString(), HashMap.class);
+					List<List<String>> detailarray = new ArrayList<List<String>>();
+					List<String> keys = new ArrayList<>(jsonObject.keySet());
+					for (String key:keys) {
+						List<String> details = new ArrayList<>();
+						details.add(key);
+						details.add(jsonObject.get(key));
+						detailarray.add(details);
+					}
+					dog.setDetail(detailarray);
+				}
+			}
 			return doginfo;
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -30,6 +48,21 @@ public class DoginfoServiceImpl implements DoginfoService {
 	public List<Doginfo> searchbyuserid(String userid) {
 		try {
 			List<Doginfo> doginfo = dao.searchbyuserid(userid);
+			for (Doginfo dog:doginfo) {
+				if (!Objects.isNull(dog.getDetail())) {
+					Gson gson = new Gson();
+					HashMap<String, String> jsonObject = gson.fromJson(dog.getDetail().toString(), HashMap.class);
+					List<List<String>> detailarray = new ArrayList<List<String>>();
+					List<String> keys = new ArrayList<>(jsonObject.keySet());
+					for (String key:keys) {
+						List<String> details = new ArrayList<>();
+						details.add(key);
+						details.add(jsonObject.get(key));
+						detailarray.add(details);
+					}
+					dog.setDetail(detailarray);
+				}
+			}
 			return doginfo;
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -41,6 +74,19 @@ public class DoginfoServiceImpl implements DoginfoService {
 	public Doginfo search(int id) {
 		try {
 			Doginfo doginfo = dao.search(id);
+			if (!Objects.isNull(doginfo.getDetail())) {
+				Gson gson = new Gson();
+				HashMap<String, String> jsonObject = gson.fromJson(doginfo.getDetail().toString(), HashMap.class);
+				List<List<String>> detailarray = new ArrayList<List<String>>();
+				List<String> keys = new ArrayList<>(jsonObject.keySet());
+				for (String key:keys) {
+					List<String> details = new ArrayList<>();
+					details.add(key);
+					details.add(jsonObject.get(key));
+					detailarray.add(details);
+				}
+				doginfo.setDetail(detailarray);
+			}
 			return doginfo;
 		} catch(Exception e) {
 			e.printStackTrace();
