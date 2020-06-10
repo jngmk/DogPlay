0a4a8cbcdac31133c20bd81d1198dae58986625a
heecheol <heecheol1508@gmail.com>
2020-06-10 16:16:08 +0900

    [feat] search hotelname by hotelnumber

--- gaenolja/src/main/java/com/gaenolja/model/service/HotelService.java
+++ gaenolja/src/main/java/com/gaenolja/model/service/HotelService.java
@@ -9,6 +9,7 @@ public interface HotelService {
 	public Hotel search(String hotelnumber);
 	public List<Hotel> searchbyname(String hotelname);
 	public List<Hotel> searchbydistance(double latitude, double longitude, int distance);
+	public String hotelnamebyhotelnumber(String hotelnumber);
 	public boolean insert(Hotel hotel);
 	public boolean update(Hotel hotel);
 	public boolean delete(String hotelnumber);
